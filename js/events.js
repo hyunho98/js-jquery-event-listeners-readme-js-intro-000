65e3553847fb68a06d67d3c64c1bf00105ce1602
Alex Lee <hyunho98@gmail.com>
2019-06-06 22:29:23 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,12 +1,14 @@
 //define functions here
 function getIt() {
-    $('p').on('click', function() {
-      alert('Hey!');
-    });
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
 }
 
 function frameIt() {
-
+  $('img').on('load', function() {
+    $('img').append($('.tasty'));
+  })
 }
 
 function pressIt() {
@@ -27,6 +29,7 @@ $(document).ready(function(){
 
 // call functions here
 getIt();
+frameIt();
 pressIt();
 submitIt();
 });
