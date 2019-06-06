73ff6a480865bf0af1d9a3942804379fac89eb2d
Alex Lee <hyunho98@gmail.com>
2019-06-06 22:22:50 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -10,10 +10,16 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('form').on('keydown', function(key){
+  $('#typing').on('keydown', function(key){
       if(key.which === 71){
         alert('g was pressed');
       }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
   })
 }
 
@@ -21,4 +27,5 @@ $(document).ready(function(){
 
 // call functions here
 getIt();
+
 });
